9416122c6f4c2cac1c1a1eefb7862401fefe2a5b
tranthuytan <thuytan03vn@gmail.com>
2025-02-03 06:33:55 +0700

    linq hands on

---
+++ CustomLinq/Program.cs
@@ -0,0 +1,32 @@
+﻿using DelegateAndLambda;
+
+namespace CustomLinq
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student { Name = "A", Age = 7 },
+                new Student { Name = "B", Age = 8 },
+                new Student { Name = "C", Age = 12 },
+                new Student { Name = "D", Age = 15 },
+                new Student { Name = "E", Age = 23 },
+                new Student { Name = "F", Age = 45 },
+                new Student { Name = "G", Age = 57 },
+            };
+            var result = students.CheckStudentAgeLessThan20(s => s.Age < 20);
+            foreach (var item in result)
+            {
+                Console.WriteLine($"Name: {item.Name} - Age: {item.Age}");
+            }
+        }
+    }
+}
---
+++ DelegateAndLambda/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace DelegateAndLambda
+{
+    internal static class Program
+    {
+        ////case 1: this code use delegate 
+        //public delegate bool CheckIntDlg(int n);
+        //public static bool CheckIntLessThan20(int n)
+        //{
+        //    return n < 20;
+        //}
+        //static IEnumerable<int> Check(List<int> ints, CheckIntDlg dlg)
+        //{
+        //    foreach (var n in ints)
+        //    {
+        //        if (dlg(n)) yield return n;
+        //    }
+        //}
+        //-----------------------------------------------------------------------------------
+        ////case 2: same code as above block but use Predicate instead of delegate and use another function as parameter
+        //public static bool CheckIntLessThan20(int n)
+        //{
+        //    return n < 20;
+        //}
+        //static IEnumerable<int> Check(List<int> ints, Predicate<int> predicate)
+        //{
+        //    foreach (var n in ints)
+        //    {
+        //        if (predicate(n)) yield return n;
+        //    }
+        //}
+        //-----------------------------------------------------------------------------------
+        //same code as above block but use Predicate instead of delegate
+        static IEnumerable<int> Check(this List<int> ints, Predicate<int> predicate)
+        {
+            foreach (var n in ints)
+            {
+                if (predicate(n)) yield return n;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var a = new List<int>() { 1, 2, 3, 4, 5, 14, 52 };
+
+            ////fucntion for case 1 and 2 (delegate)
+            //var b = Check(a, CheckIntLessThan20);
+
+            ////function for case 3 (anonymous function)
+            //var b = Check(a, (int n) => { return n < 20; });
+
+            //function for case 3 (but with lamda expression)
+            var b = Check(a, n => n < 20);
+
+            foreach (var item in b)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
